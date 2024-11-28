ff63bcf4e77ab5656507d12d73be86cf4345ed8a
jordela1 <jo_delahunty@hotmail.com>
2024-11-27 22:28:03 -0600

    Started creating main menu generator

--- utils/common.py
+++ utils/common.py
@@ -10,3 +10,26 @@ def said_yes( question: str ) -> bool:
             return False
         else:
             print("Incorrect response, please enter 'yes/y' or 'no/n'")
+
+def menu_maker(menu_title: str, menu_desc) -> str:
+    max_menu_len = 50
+    menu_title_len = len(menu_title)
+    menu_desc_len = len(menu_desc)
+    menu_desc_rows = []
+
+    # If the length of the provided title is < the default max, 
+    # but is odd will append a space to make the buffers even on either side
+    # If the provided title is greater than default max will set the default max to that length
+    if menu_title_len < max_menu_len and menu_title_len % 2 != 0:
+        menu_title + " "
+    elif menu_title_len > max_menu_len:
+        max_menu_len = menu_title_len
+
+    # If the length of the provided desc is > the current max will split into rows of that length
+    if menu_desc_len > max_menu_len:
+        menu_desc_list = menu_desc.split()
+        w
+
+
+
+
